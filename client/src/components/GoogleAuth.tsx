c50b2f42b07f1e1d6331d06f1fa16e359aa39b9a
Gareth <gareth.kh.lau@gmail.com>
2019-12-22 01:43:16 -0500

    + authnetication works

--- client/src/components/GoogleAuth.tsx
+++ client/src/components/GoogleAuth.tsx
@@ -1,11 +1,66 @@
-import React from 'react';
+import React from "react";
+import { Button } from "@material-ui/core";
+import axios from "axios";
+interface Event {
+    class: String;
+    code: String;
+    section: String;
+    type: String;
+    day: String;
+    start: String;
+    end: String;
+    location: String;
+  }
 
-const GoogleAuth: React.FC = () => {
-    return (
-        <div>
-            Google Auth Page
-        </div>
-    )
-}
+const GoogleAuth: React.FC<{
+    options: {
+        startDate: String,
+        endDate: String,
+        calendarId: String
+    },
+    events: Event[]
+}> = (props) => {
+
+  const startAuth = () => {
+    axios
+      .get("/login", {
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        }
+      })
+      .then((res) => {
+        console.log("done login??", res);
+        window.open(res.data, "_blank")
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  const createEvents = () => {
+      const data = new FormData();
+      data.append('options', JSON.stringify(props.options))
+      data.append('events', JSON.stringify(props.events))
+      axios.post('/api/v1/create-events', data).then(res => {
+          console.log(res)
+      }).catch(err => {
+          console.log(err)
+      })
+  }
+
+  return (
+    <div>
+      Google Auth Page
+      <Button variant="contained" color="primary" onClick={startAuth}>
+        Login to Google
+      </Button>
+
+
+      <Button variant="contained" color="primary" onClick={createEvents}>
+        Create Events
+      </Button>
+    </div>
+  );
+};
 
 export default GoogleAuth;
