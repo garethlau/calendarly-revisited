d36488be21c18bab2e0880abf7ffed4cc505f199
Gareth <gareth.kh.lau@gmail.com>
2019-12-23 22:19:19 -0500

    add build to commit

---
+++ client/build/precache-manifest.e952bda7471c953406807f8dc2cf0236.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a24a3c112bb48409d1fc492366e1a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b632c9451ebd026a401",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c42dfdc8402a523a8943",
+    "url": "/static/js/2.047bfaf0.chunk.js"
+  },
+  {
+    "revision": "b4388488e6778855973fb4c6caca93fc",
+    "url": "/static/js/2.047bfaf0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b632c9451ebd026a401",
+    "url": "/static/js/main.2c3762c7.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  },
+  {
+    "revision": "34462a7d4ca405eae23eabcfa7cfbbbc",
+    "url": "/static/media/created.34462a7d.svg"
+  },
+  {
+    "revision": "394655bd6a19ab1ab45e528e67a65476",
+    "url": "/static/media/empty.394655bd.svg"
+  },
+  {
+    "revision": "9572ce9515c3c3c1a7addc1804980b7c",
+    "url": "/static/media/failure.9572ce95.svg"
+  },
+  {
+    "revision": "8fcca73dfba72f8c9394bb6a95176560",
+    "url": "/static/media/file-drop.8fcca73d.svg"
+  },
+  {
+    "revision": "ff28c8a4f50fe6eef737daf61fe66674",
+    "url": "/static/media/not-found.ff28c8a4.svg"
+  },
+  {
+    "revision": "9f4f9efbb6cd5e8839add6659a32eaca",
+    "url": "/static/media/secure.9f4f9efb.svg"
+  },
+  {
+    "revision": "740177cf20d99c6cb5bb6d860549a7cc",
+    "url": "/static/media/settings.740177cf.svg"
+  },
+  {
+    "revision": "e64042a077c6d6d80733b463dc19af41",
+    "url": "/static/media/success.e64042a0.svg"
+  }
+]);
